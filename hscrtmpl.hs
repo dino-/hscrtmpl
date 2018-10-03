2254274aa1a8f56fa66b84376b446340ce16cdec
Dino Morelli <dino@ui3.info>
2018-10-03 11:19:19 -0400

    Changed to two-space indenting

--- hscrtmpl.hs
+++ hscrtmpl.hs
@@ -5,26 +5,27 @@
 #! /usr/bin/env runhaskell
 -}
 
-{- This is a 'skeleton file' for writing shell scripts with Haskell
+{-
+  This is a 'skeleton file' for writing shell scripts with Haskell
 
-   Haskell is my go-to language for anything more complicated than a
-   simple sequence of shell commands in a bash script. Doing list
-   processing and a lot of logic in bash is a grind, to me.
+  Haskell is my go-to language for anything more complicated than a simple
+  sequence of shell commands in a bash script. Doing list processing and a lot
+  of logic in bash is a grind, to me.
 
-   The idea here is to take a copy of this script and hack it to do
-   what you need quickly. Throw the rest out.
+  The idea here is to take a copy of this script and hack it to do what you
+  need quickly. Throw the rest out.
 
-   The script starts off (after the imports) with a main full of
-   examples of common things along with their bash counterparts.
+  The script starts off (after the imports) with a main full of examples of
+  common things along with their bash counterparts.
 
-   After that are a few functions that simplify things like getting
-   the date as a String, logging a date-stamped String to stdout and
-   manupulating an ExitCode as a true/false value.
+  After that are a few functions that simplify things like getting the date as
+  a String, logging a date-stamped String to stdout and manupulating an
+  ExitCode as a true/false value.
 
 
-   Dino Morelli <dino@ui3.info>
-   https://github.com/dino-/hscrtmpl
-   version: 1.6
+  Dino Morelli <dino@ui3.info>
+  https://github.com/dino-/hscrtmpl
+  version: 1.6
 -}
 
 import Control.Monad
@@ -43,64 +44,64 @@ import Text.Printf
 
 main :: IO ()
 main = do
-   putStrLn "This is a shell script"
+  putStrLn "This is a shell script"
 
                                              -- in bash:
-   -- dates (Date.Time)
-   putStrLn =<< date                         -- date
-   putStrLn =<< dateFormat "%Y%m%d"          -- date +"%Y%m%d"
-      -- These two functions below
+  -- dates (Date.Time)
+  putStrLn =<< date                         -- date
+  putStrLn =<< dateFormat "%Y%m%d"          -- date +"%Y%m%d"
+    -- These two functions below
 
-   -- file/dir things
-   --   (System.Directory)
-   putStrLn =<< getHomeDirectory             -- echo $HOME
-   print =<< doesFileExist "foo"             -- [ -f foo ]
-   print =<< doesDirectoryExist "bar"        -- [ -d bar ]
-   putStrLn =<< getCurrentDirectory          -- pwd
-   --setCurrentDirectory "/tmp"                -- cd /tmp
-   print =<< makeAbsolute "some/relative/path"
-   --   (System.FilePath)
-   let (path, ext) = splitExtension "foo/bar.baz"
-   print (path, ext)
+  -- file/dir things
+  --   (System.Directory)
+  putStrLn =<< getHomeDirectory             -- echo $HOME
+  print =<< doesFileExist "foo"             -- [ -f foo ]
+  print =<< doesDirectoryExist "bar"        -- [ -d bar ]
+  putStrLn =<< getCurrentDirectory          -- pwd
+  --setCurrentDirectory "/tmp"                -- cd /tmp
+  print =<< makeAbsolute "some/relative/path"
+  --   (System.FilePath)
+  let (path, ext) = splitExtension "foo/bar.baz"
+  print (path, ext)
 
-   -- conditional statements (Control.Monad)
-   e <- doesFileExist "hscrtmpl.hs"          -- [ -f hscrtmpl.hs ]
-   when e $ putStrLn "This script exists!"   --    && echo "This script exists!"
+  -- conditional statements (Control.Monad)
+  e <- doesFileExist "hscrtmpl.hs"          -- [ -f hscrtmpl.hs ]
+  when e $ putStrLn "This script exists!"   --    && echo "This script exists!"
    
 
-   -- environment variables (System.Environment)
-   putStrLn =<< getEnv "SHELL"               -- echo $SHELL
+  -- environment variables (System.Environment)
+  putStrLn =<< getEnv "SHELL"               -- echo $SHELL
 
-   -- arguments (System.Environment)
-   --(arg1 : arg2 : _) <- getArgs              -- arg1=$1 ; arg2=$2
+  -- arguments (System.Environment)
+  --(arg1 : arg2 : _) <- getArgs              -- arg1=$1 ; arg2=$2
 
-   -- string interpolation (Text.Printf)
-   printf "The %s is %d\n"                   -- S="answer" ; D=42
-      "answer" (42::Int)                     -- echo "The $S is $D"
+  -- string interpolation (Text.Printf)
+  printf "The %s is %d\n"                   -- S="answer" ; D=42
+    "answer" (42::Int)                     -- echo "The $S is $D"
 
-   -- execution, exit code (System.Process)
-   ec <- system "ls -l"                      -- ls -l
-   print ec                                  -- echo $?
+  -- execution, exit code (System.Process)
+  ec <- system "ls -l"                      -- ls -l
+  print ec                                  -- echo $?
 
-   -- execution, capture stdout (System.Process)
-   output <- readProcess "ls" ["-l"]         -- output=$(ls -l)
-      "stdin data, if desired"
+  -- execution, capture stdout (System.Process)
+  output <- readProcess "ls" ["-l"]         -- output=$(ls -l)
+    "stdin data, if desired"
 
-      -- or use readProcessWithExitCode
-      -- :: FilePath -> [String] -> String -> IO (ExitCode, String, String)
-      --    program     args        stdin         exitcode  stdout  stderr
+    -- or use readProcessWithExitCode
+    -- :: FilePath -> [String] -> String -> IO (ExitCode, String, String)
+    --    program     args        stdin         exitcode  stdout  stderr
 
-   -- regular expressions (Text.Regex)
-   --print $ matchRegex                        -- (see bash docs for =~
-   --   (mkRegex "a(.)b(.)") "axby"            --  and BASH_REMATCH)
+  -- regular expressions (Text.Regex)
+  --print $ matchRegex                        -- (see bash docs for =~
+  --   (mkRegex "a(.)b(.)") "axby"            --  and BASH_REMATCH)
 
-   -- Handy date-stamping logM function below
-   logM "Example of a log message"
+  -- Handy date-stamping logM function below
+  logM "Example of a log message"
 
-   -- exiting (System.Exit)
-   exitSuccess                               -- exit 0
-   --exitFailure                             -- exit 1
-   --exitWith $ ExitFailure 3                -- exit 3
+  -- exiting (System.Exit)
+  exitSuccess                               -- exit 0
+  --exitFailure                             -- exit 1
+  --exitWith $ ExitFailure 3                -- exit 3
 
 
 {- Get the current date/time as a string in RFC822 format
@@ -153,7 +154,7 @@ exitBool = exitWith . toExitCode
 -}
 forSystem :: [String] -> IO ExitCode
 forSystem cs = do
-   ecs <- mapM system cs
-   return $ case all (== ExitSuccess) ecs of
-      True  -> ExitSuccess
-      False -> ExitFailure 1
+  ecs <- mapM system cs
+  return $ case all (== ExitSuccess) ecs of
+    True  -> ExitSuccess
+    False -> ExitFailure 1
